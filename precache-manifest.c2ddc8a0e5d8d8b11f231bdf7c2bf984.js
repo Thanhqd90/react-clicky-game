97ee19a1f4efb17ea4fd122be47e0dac998c1383
Thanh Dang <Thanhqd1990@gmail.com>
2019-01-09 17:42:16 -0600

    Updates

---
+++ precache-manifest.c2ddc8a0e5d8d8b11f231bdf7c2bf984.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "55abe30a270b3be75741",
+    "url": "/react-clicky-game/static/js/runtime~main.55abe30a.js"
+  },
+  {
+    "revision": "b70fcf6dc4c599642d56",
+    "url": "/react-clicky-game/static/js/main.b70fcf6d.chunk.js"
+  },
+  {
+    "revision": "aac94eec18fdc6950f2f",
+    "url": "/react-clicky-game/static/js/1.aac94eec.chunk.js"
+  },
+  {
+    "revision": "b70fcf6dc4c599642d56",
+    "url": "/react-clicky-game/static/css/main.ffe70e68.chunk.css"
+  },
+  {
+    "revision": "58ba8a2c7cd3db4706ba9f215f15f6f3",
+    "url": "/react-clicky-game/index.html"
+  }
+];
