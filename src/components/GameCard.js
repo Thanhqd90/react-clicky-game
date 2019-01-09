6eb779f52bfaa913bdb1514d2db1e4f00a9daa1a
Thanh Dang <Thanhqd1990@gmail.com>
2019-01-09 17:32:18 -0600

    Fixed mobile view

--- src/components/GameCard.js
+++ src/components/GameCard.js
@@ -4,7 +4,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import './GameCard.css'
 
 const GameCard = props => (
-    <Col s={12} m={4} l={3}>
+    <Col s={6} m={4} l={3}>
         <CardPanel onClick={() => props.clickHandler(props.icon.iconName)} className={"hoverable black lighten-4 white-text center" + (props.correct === false ? " shake" : "")}>
             <FontAwesomeIcon icon={props.icon} />
         </CardPanel>
